30a6654855f8bb1f7d4392e11f1b0714108a1b58
moldovanjason <moldovanjason@gmail.com>
2020-08-06 19:35:45 +0000

    5th

--- src/js/components/card.js
+++ src/js/components/card.js
@@ -1,16 +1,19 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 //create your first component
-export const Card = () => {
+export const Card = props => {
 	return (
-		<div className="card" style={{ width: "18rem" }}>
+		<div className="card" key={props.keyIndex} style={{ width: "18rem" }}>
 			<img src="..." className="card-img-top" alt="..." />
 			<div className="card-body">
-				<p className="card-text">
-					Some quick example text to build on the card title and make
-					up the bulk of the cards content.
-				</p>
+				<p className="card-text">{props.character.name}</p>
 			</div>
 		</div>
 	);
+};
+
+Card.propTypes = {
+	character: PropTypes.object,
+	keyIndex: PropTypes.number
 };
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -6,17 +6,36 @@ import { Card } from "../components/card";
 export class Home extends React.Component {
 	constructor() {
 		super();
-		this.state = {};
+		this.state = {
+			char: [],
+			planets: []
+		};
+	}
+
+	componentDidMount() {
+		fetch("https://swapi.dev/api/people/")
+			.then(response => response.json())
+			.then(characters => this.setState({ char: characters.results }));
 	}
 
 	render() {
+        const charCards;
+        if(this.state.char.length>1){
+            charCards = this.state.char.map((character, cardIndex) => {
+			return <Card key={cardIndex} character={character} />;
+		});
+        }
+		else {
+            console.log("loading...")
+        }
+
 		return (
 			<div className="container-fluid">
 				<Navbar />
-				<Card />
+
 				<div className="heading" />
 				<div className="navBar">B</div>
-				<div className="cards">C</div>
+				<div className="cards">{charCards}</div>
 			</div>
 		);
 	}
