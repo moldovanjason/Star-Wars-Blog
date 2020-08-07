7362004258a5c54070138ffef6c5c47d0627e774
moldovanjason <moldovanjason@gmail.com>
2020-08-07 14:09:34 +0000

    9th

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -18,15 +18,6 @@ export class Home extends React.Component {
 	}
 
 	render() {
-		// var charCards = null;
-		// if (this.state.char.length > 1) {
-		// 	charCards = this.state.char.map((character, cardIndex) => {
-		// 		return <Card key={cardIndex} name={character.name} />;
-		// 	});
-		// } else {
-		// 	// console.log("loading...");
-		// }
-
 		return (
 			<div className="container-fluid">
 				<Navbar />
